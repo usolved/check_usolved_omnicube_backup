02927cdc1bbee2be9c2c0bceca66b16ac7de1ba2
USOLVED <git@usolved.net>
2014-12-09 10:22:55 +0100

    First release

---
+++ README.md
@@ -0,0 +1,150 @@
+# check_usolved_omnicube_backup
+
+## Overview
+
+This Python Nagios plugin checks for any failed VM backups on SimpliVity Omnicubes. 
+There's also a second mode in this plugin to check if all VMs have assigned backup policies.
+
+## Authors
+
+Ricardo Klement (www.usolved.net)
+
+## Installation
+
+Just copy the file check_usolved_omnicube_backup.py into your Nagios plugin directory.
+For example this path: /usr/local/nagios/libexec/
+
+Give check_usolved_omnicube_backup.py execution rights for the nagios user.
+This plugin needs Python 2 to be installed.
+
+Because this plugin uses SSH to connect to the OmniCube you need the Python module "pexpect" installed which contains "pxssh".
+
+<pre><code>
+sudo yum install pexpect.noarch
+</code></pre>
+or
+<pre><code>
+sudo apt-get install python-pexpect
+</code></pre>
+
+Why not Python 3 you may ask?
+Most Nagios / Icinga installations are already using other plugins which are written in Python 2.
+So for compatibility reasons i've decided to use Python 2 as well.
+
+
+## Usage
+
+### Test on command line
+If you are in the Nagios plugin directory execute this command:
+
+<pre><code>
+./check_usolved_omnicube_backup.py -H ip_address_of_omnicube -U "domain\\\myuser" -P yourpassword -M status
+</code></pre>
+
+This should output something like this:
+
+<pre><code>
+OK - All backups were successful
+</code></pre>
+
+Here are all arguments that can be used within this plugin:
+
+<pre><code>
+-H &lt;host address&gt;
+Give the host address of an OmniCube with the IP address or FQDN
+
+-U &lt;ssh user&gt;
+OmniCube SSH username (remember to escape the backslash in the username. For example domain\myuser would be "domain\\\\\myuser" or domain\\\\\\\myuser as argument)
+
+-P &lt;ssh password&gt;
+OmniCube SSH password
+
+-M &lt;mode&gt;
+Plugin mode (-M status (to check if all backups were successful) or -M policy (to check if all VMs have policies assigned))
+
+[-E &lt;exclude VMs&gt;]
+Optional: Exclude comma separated hosts from the check for policy check
+
+[-D &lt;perfdata&gt;]
+Optional: Without an argument the backups status check gets the status from yesterday.
+If you wish to check for other time ranges, give the argument -D YYYY-MM-DD
+
+[-T &lt;perfdata&gt;]
+Optional: SSH timeout in seconds. Default is 45 seconds.
+</code></pre>
+
+### Install in Nagios
+
+Edit your **commands.cfg** and add the following.
+
+Example for checking the backup status:
+
+<pre><code>
+define command {
+    command_name    check_usolved_omnicube_backup_status
+    command_line    $USER1$/check_usolved_omnicube_backup.py -H $HOSTADDRESS$ -U $ARG1$ -P $ARG2$ -M status
+}
+</code></pre>
+
+Example for checking the assigned backup policies:
+
+<pre><code>
+define command {
+    command_name    check_usolved_omnicube_backup_policy
+    command_line    $USER1$/check_usolved_omnicube_backup.py -H $HOSTADDRESS$ -U $ARG1$ -P $ARG2$ -M policy -E $ARG3$
+}
+</code></pre>
+
+Edit your **services.cfg** and add the following.
+
+Example for checking the backup status:
+
+<pre><code>
+define service{
+	host_name				Test-Server
+	service_description		OmniCube-Backup-Status
+	use						generic-service
+	check_command			check_usolved_omnicube_backup_status!domain\\\\username!password
+}
+</code></pre>
+
+Example for checking the assigned backup policies:
+
+<pre><code>
+define service{
+	host_name				Test-Server
+	service_description		OmniCube-Backup-Policies
+	use						generic-service
+	check_command			check_usolved_omnicube_backup_policy!domain\\\\username!password!hostnotwant1,hostnotwant2
+}
+</code></pre>
+
+You could also use host macros for the username and password.
+Again: Pay attention for escaping the domain and username with the backslashes.
+If you use a frontend like Centreon you need to put 4 backslashes between the domain and username.
+
+Don't worry if the check takes a while for returning the info. The OmniCube SSH commands will take a while for executing.
+
+
+### Errors you may encounter
+
+If you get some cryptic erorrs with pxssh on CentOS 6 you need to edit the fallowing file:
+
+<pre><code>
+vi /usr/lib/python2.6/site-packages/pxssh.py
+</code></pre>
+
+
+Add the following lines of code:
+
+<pre><code>
+self.sendline() #Line 134
+time.sleep(0.5) #Line 135
+</code></pre>
+
+
+Should be inserted  above the fallowing line:
+
+<pre><code>
+self.read_nonblocking(size=10000,timeout=1) # GAS: Clear out the cache before getting the prompt
+</code></pre>
---
+++ check_usolved_omnicube_backup.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import optparse
+import time
+import datetime
+import pxssh
+
+try:
+	import xml.etree.ElementTree as et
+except ImportError:
+	import elementtree.ElementTree as et
+
+
+######################################################################
+# Definitions of variables
+
+# Arrays for return codes and return message
+return_code 	= { 'OK': 0, 'WARNING': 1, 'CRITICAL': 2, 'UNKNOWN': 3 }
+return_msg 		= ''
+return_perfdata = ''
+hosts_excluded	= ''
+
+
+
+######################################################################
+# Parse Arguments
+parser 		= optparse.OptionParser()
+parser.add_option('-H', '--hostname', help='IP or hostname of the OmniCube host', dest='arg_hostname', type='string')
+parser.add_option('-U', '--username', help='OmniCube SSH username (remember to escape the backslash in the username. For example domain\myuser would be "domain\\\\\myuser" or domain\\\\\\\myuser as argument)', dest='arg_username', type='string')
+parser.add_option('-P', '--password', help='OmniCube SSH password', dest='arg_password', type='string')
+parser.add_option('-M', '--mode', help='Plugins mode (-M status (to check if all backups were successful) or -M policy (to check if all VMs have policies assigned))', dest='arg_mode', type='string')
+parser.add_option('-E', '--exclude', help='Exclude comma separated hosts for policy check', dest='arg_exclude', type='string', default='')
+parser.add_option('-D', '--backupdate', help='Without an argument the backups status check gets the status from yesterday. If you wish to check for other days, give the argument -D YYYY-MM-DD', dest='arg_backupdate', type='string', default='yesterday')
+parser.add_option('-T', '--timeout', help='SSH timeout in seconds', dest='arg_timeout', type='int', default=45)
+(opts, args) = parser.parse_args()
+
+arg_hostname 		= opts.arg_hostname
+arg_username		= opts.arg_username
+arg_password		= opts.arg_password
+arg_mode			= opts.arg_mode
+arg_exclude			= opts.arg_exclude
+arg_backupdate		= opts.arg_backupdate
+arg_timeout			= opts.arg_timeout
+
+if arg_exclude != "":
+	hosts_excluded = arg_exclude.split(',')
+
+######################################################################
+# Functions
+
+def output_nagios(return_msg, return_perfdata, return_code):
+	print return_msg
+	sys.exit(return_code)
+
+
+####################################################
+
+def ssh_connect(hostname, username, password, timeout):
+
+	global return_msg
+
+	try:
+		ssh = pxssh.pxssh(timeout=timeout)
+		ssh.login(hostname, username, password)
+		ssh.prompt()
+		return ssh
+
+	except pxssh.ExceptionPxssh, e:
+		return_msg = "Unknown - pxssh failed on login. "
+		return_msg += str(e)
+		output_nagios(return_msg,'',return_code['UNKNOWN'])
+
+def ssh_logout():
+	ssh.logout()
+
+
+####################################################
+
+def get_failed_backups():
+
+	if arg_backupdate == "yesterday":
+		time_current 			= int(time.time()) - 86400
+		time_current_date	 	= datetime.datetime.fromtimestamp(time_current).strftime('%Y-%m-%d')
+	else:
+		time_current_date = arg_backupdate
+
+	ssh.sendline('svt-backup-show --status failed --output xml --since '+time_current_date)
+	ssh.prompt()
+	data = ssh.before
+	data, rest = data.split('\n', 1) #strip out command itself
+	#data = data.splitlines()
+	return rest
+
+
+
+def get_failed_backups_status(hosts_failed_backups):
+
+	global return_msg
+
+	return_hosts	= ''
+	backup_status 	= 0
+
+	try:
+		hosts_failed_backups_xml = et.fromstring(hosts_failed_backups)
+		for child in hosts_failed_backups_xml.findall('Backup'):
+
+			failed_timestamp = int(child.find('timestamp').text)
+			failed_timestamp = datetime.datetime.fromtimestamp(failed_timestamp).strftime('%Y-%m-%d %H:%M')
+
+			return_hosts += child.find('hiveName').text+" ("+ str(failed_timestamp)+"), "
+			backup_status = 2
+
+
+		if backup_status == 0:
+			return_msg = 'OK - All backups were successful'
+		else:
+			return_hosts 	= return_hosts[:-2]
+			return_msg 		= 'Critical - Backup for '+return_hosts+' failed'
+
+		return backup_status
+	except:
+		return_msg = 'Unknown - Returned XML data is not valid'
+		return 3
+
+####################################################
+
+def get_hosts_with_no_policy():
+	ssh.sendline('svt-vm-show --output xml')
+	ssh.prompt()
+	data = ssh.before
+	data, rest = data.split('\n', 1) #strip out command itself
+	#data = data.splitlines()
+	return rest
+
+
+def get_hosts_with_no_policy_status(hosts_with_no_policy):
+
+	global return_msg
+	return_hosts		= ''
+	no_policy_status 	= 0
+
+	try:
+		hosts_with_no_policy_xml = et.fromstring(hosts_with_no_policy)
+		for child in hosts_with_no_policy_xml.findall('VM'):
+
+			if child.find('policy').text == "empty":
+				if child.find('platformName').text not in hosts_excluded:
+					return_hosts += child.find('platformName').text+", "
+					no_policy_status = 2		
+
+
+		if no_policy_status == 0:
+			return_msg = 'OK - Backup policies for all hosts are configured'
+		else:
+			return_hosts 	= return_hosts[:-2]
+			return_msg 		= 'Critical - Backup policy for '+return_hosts+' is missing'
+
+		return no_policy_status
+	except:
+		return_msg = 'Unknown - Returned XML data is not valid'
+		return 3
+
+
+
+######################################################################
+# General
+
+
+if arg_mode == "status":
+	ssh 					= ssh_connect(arg_hostname, arg_username, arg_password, arg_timeout)
+	hosts_failed_backups 	= get_failed_backups()
+
+	backup_status 			= get_failed_backups_status(hosts_failed_backups)
+
+	if backup_status == 0:
+		output_nagios(return_msg,'',return_code['OK'])
+	elif backup_status == 2:
+		output_nagios(return_msg,'',return_code['CRITICAL'])
+	else:
+		output_nagios(return_msg,'',return_code['UNKNOWN'])
+
+	#ssh_logout()
+elif arg_mode == "policy":
+	ssh 					= ssh_connect(arg_hostname, arg_username, arg_password, arg_timeout)
+	hosts_with_no_policy 	= get_hosts_with_no_policy()
+
+	no_policy_status 		= get_hosts_with_no_policy_status(hosts_with_no_policy)
+
+	if no_policy_status == 0:
+		output_nagios(return_msg,'',return_code['OK'])
+	elif no_policy_status == 2:
+		output_nagios(return_msg,'',return_code['CRITICAL'])
+	else:
+		output_nagios(return_msg,'',return_code['UNKNOWN'])
+
+	#ssh_logout()
+else:
+	return_msg = 'Unknown - Please select a mode.\nType ./'+os.path.basename(__file__)+' --help for all options.'
+	output_nagios(return_msg,'',return_code['UNKNOWN'])
+	
